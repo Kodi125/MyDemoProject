94aa40139a5e5f396b02b98632db5207bd055da7
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-03-03 02:32:37 +0000

    Add project files.

---
+++ MyDemoProject.Test/testCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyDemoProject;
+
+namespace MyDemoProject.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public static void Test_1()
+        {
+            //arrange
+            Program p1 = new Program();
+            //act
+            int actualResult = p1.Add(5, 10);
+            //assert
+            Assert.That(actualResult, Is.EqualTo(15));  //15 is the expected result
+        }
+        [Test]
+        public static void Test_2()
+        {
+            //arrange
+            Program p1 = new Program();
+            //act
+            int actualResult = p1.Add(8, 14);
+            //assert
+            Assert.That(actualResult, Is.EqualTo(21));  //22 is the expected result
+        }
+    }
+}
---
+++ MyDemoProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDemoProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //manual data entry for testing
+            //Console.WriteLine(Add(5,8));
+
+
+        }
+        public int Add(int a, int b)
+        {
+            int actualResult;
+
+            actualResult = a + b;
+
+            return actualResult;
+        }
+    }
+}
